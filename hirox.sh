9c17c18b31bf4dd10de2e6c7ef773ee24ad05f08
Bekama2004 <120168969+Bekama2004@users.noreply.github.com>
2023-06-29 18:40:21 +0530

    Add files via upload

---
+++ hirox.sh
@@ -0,0 +1,13 @@
+while : 
+do
+echo "CARREGANDO O SCRIPT"
+clear
+echo "CARREGANDO O SCRIPT."
+clear
+echo "CARREGANDO O SCRIPT.."
+clear
+echo "CARREGANDO O SCRIPT..."
+    node index.js
+    sleep 1
+
+done
